12dcbd0e22b2a9bbebe91dffa920212eb960ee55
Danial-Doskuzhinov <doskuzhinovdanial@gmail.com>
2024-07-11 14:34:21 +0500

    fix deploy

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export {default as Html} from './icon-html.svg';
+export {default as Css} from './icon-css (1).svg';
+export {default as Js} from './icon-js (1).svg';
+export {default as Accessibility} from './icon-accessibility (1).svg';
+
--- src/component/QuizSelection.tsx
+++ src/component/QuizSelection.tsx
@@ -1,27 +1,41 @@
 import React from 'react';
+import { Html, Css, Js, Accessibility } from '../assets/index';
 
 type QuizSelectionProps = {
-    quizzes: { title: string ,icon:string}[];
+    quizzes: { title: string, icon: string }[];
     handleStartQuiz: (quizTitle: string) => void;
 };
 
 const QuizSelection: React.FC<QuizSelectionProps> = ({ quizzes, handleStartQuiz }) => {
- 
+    const logo = [
+        { id: 0, icon: Html, name: 'Html' },
+        { id: 1, icon: Css, name: 'Css' },
+        { id: 2, icon: Js, name: 'Js' },
+        { id: 3, icon: Accessibility , name: 'Accessibility' }
+    ];
+
+    const getIconComponent = (iconName: string) => {
+        const logoItem = logo.find(logo => logo.name === iconName);
+        return logoItem ? logoItem.icon :'';
+    };
+
     return (
         <div className='flex'>
-            
-            <h2 className='text-[65px] ml-[7rem] w-[470px] mr-[6rem] mt-[10rem]'>Welcome to the Frontend Quiz!</h2>
+            <h2 className='text-[65px] ml-[7rem] w-[470px] mr-[6rem] mt-[10rem]'>Welcome to the Frontend Quiz!
+</h2>
+<br />            <p className='text-gray-900'>better turn dark mode</p>
+
             <div className='flex-col mt-[6rem]'>
-
-            {quizzes.map((quiz, index) => (
-                <button key={index}  onClick={() => handleStartQuiz(quiz.title)}
-                className='w-[450px] h-[85px] flex items-center font-bold  pl-[3rem] mb-2 rounded-lg  custom-cursor  text-[25px] bg-gray-700 hover:bg-purple-600'>
-                    <img className='pr-[2rem]' src={quiz.icon} alt={`${quiz.title}`} />
-                    {quiz.title}
-                </button>
-            ))}
+                {quizzes.map((quiz, index) => (
+                    <button key={index} onClick={() => handleStartQuiz(quiz.title)}
+                        className='w-[450px] h-[85px] flex items-center font-bold pl-[3rem] mb-2 rounded-lg custom-cursor text-[25px] bg-gray-700 hover:bg-purple-600'>
+                        <div className='pr-[2rem]'>
+                            <img src={getIconComponent(quiz.icon)} alt="" />
+                        </div>
+                        {quiz.title}
+                    </button>
+                ))}
             </div>
-
         </div>
     );
 };
