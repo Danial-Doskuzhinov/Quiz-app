8a7f11404229978acb47b0f8854c4da9390e2c53
Danial-Doskuzhinov <doskuzhinovdanial@gmail.com>
2024-07-11 19:01:58 +0500

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import { ThemeProvider } from './Context/ThemeContext';
 import QuizHeader from './component/header';
-import { BrowserRouter as Router } from 'react-router-dom';
-
+//
 const QuizApp = () => {
 
     return (
         <>
-         <Router>
             <ThemeProvider>
             <QuizHeader />
-
             </ThemeProvider>
-        </Router>
         </>
     );
 };
